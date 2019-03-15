533b9b3f77efdcc665739f4bf2183bbfa2206822
JinZhang <2636423765@qq.com>
2019-03-15 20:26:40 +0800

    refactor:更新默认编码格式

--- assets/main.py
+++ assets/main.py
@@ -2,9 +2,11 @@
 # @Author: JimDreamHeart
 # @Date:   2018-04-19 11:59:45
 # @Last Modified by:   JinZhang
-# @Last Modified time: 2019-03-15 18:59:38
-
-import os;
+# @Last Modified time: 2019-03-15 20:04:02
+import sys,os;
+# 设置默认编码格式
+reload(sys);
+sys.setdefaultencoding('utf-8');
 
 from common._load import Loader;
 from launcher._load import LauncherLoader;
