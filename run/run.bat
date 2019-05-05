921c163244536407c9d266548b4ef1a37f3e7c74
JimDreamHeart <15602291936@163.com>
2019-05-06 00:49:33 +0800

    refactor:修改运行脚本逻辑【读取python路径】

--- run/run.bat
+++ run/run.bat
@@ -1,12 +1,44 @@
 @REM @Author: JinZhang
 @REM @Date:   2018-04-19 17:16:41
-@REM @Last Modified by:   JimDreamHeart
-@REM Modified time: 2018-11-24 11:18:49
+@REM @Last Modified by:   JimZhang
+@REM Modified time: 2019-05-06 00:33:48
 
 @echo off && setlocal enabledelayedexpansion
 
+set so=
+set pypath=
+
+for /f "delims=" %%a in (config.ini) do (
+	set v=%%a
+	if "!v:~0,1!"=="[" (
+		if %%a==[env] (
+			set so=1
+		) else (
+			set so=
+		)
+	) else (
+		if defined so (
+			for %%b in (%%a) do (
+				if %%b==python (
+					set so=2
+				) else (
+					if !so!==2 (
+						set pypath=%%b&&goto readover
+					)
+				)
+			)
+		)
+	)
+)
+
+:readover
+
 cd ..\assets\
 
-python main.py
+if defined pypath (
+	%pypath%\python.exe main.py
+) else (
+	python main.py
+)
 
 pause
