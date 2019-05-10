f442706b15b47a2bf1bdc4fd04288c48b2adbf7f
JinZhang <2636423765@qq.com>
2019-05-10 17:00:09 +0800

    refactor:修改客户端运行脚本

--- run/run.bat
+++ run/run.bat
@@ -1,14 +1,14 @@
 @REM @Author: JinZhang
 @REM @Date:   2018-04-19 17:16:41
 @REM @Last Modified by:   JinZhang
-@REM Modified time: 2019-05-10 14:42:05
+@REM Modified time: 2019-05-10 16:58:14
 
 @echo off && setlocal enabledelayedexpansion
 
+rem 获取python路径
 set so=
 set pypath=
-
-for /f "delims=" %%a in (config.ini) do (
+for /f "delims=" %%a in (..\assets\common\config\ini\config.ini) do (
 	set v=%%a
 	if "!v:~0,1!"=="[" (
 		if %%a==[env] (
@@ -33,18 +33,47 @@ for /f "delims=" %%a in (config.ini) do (
 
 :readover
 
+rem 获取运行文件名
 set mainFile="main.pyc"
-
 if exist ..\assets\main.py (
 	set mainFile="main.py"
 )
 
+rem 获取运行程序
+set pythonExe="python"
+if defined pypath (
+	set pythonExe=%pypath%"\python.exe"
+)
+
+rem 判断是否安装了wxPython
+for /f "tokens=2 delims=: " %%i in ('%pythonExe% -m pip show wxPython') do (
+	if %%i==wxPython (
+		goto existed
+	)
+)
+
+:installwx
+echo "Installing wxPython ..."%pythonExe%
+for /f "tokens=2 delims=: " %%i in ('%pythonExe% -m pip show wxPython') do (
+	if %%i==wxPython (
+		goto existed
+	)
+)
+set /p isInstall=Failed to install wxPython, do you try again ?(y/n):
+if %isInstall%==y (
+	goto installwx
+) else (
+	echo "Quit to install wxPython !"
+	goto endRun
+)
+
+:existed
+echo "Existed wxPython ."
+
 cd ..\assets\
 
-if defined pypath (
-	%pypath%\python.exe %mainFile%
-) else (
-	python %mainFile%
-)
+%pythonExe% %mainFile%
+
+:endRun
 
 pause
