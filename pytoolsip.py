5857c3adfc6675990a1096b605a6323587e82a89
JimDreamHeart <15602291936@163.com>
2019-08-24 16:58:33 +0800

    refactor:新增初始化工程依赖模块数据的方法

--- pytoolsip.py
+++ pytoolsip.py
@@ -4,7 +4,7 @@
 # @Last Modified by:   JimDreamHeart
 # @Last Modified time: 2019-04-20 00:00:41
 
-import os,re,subprocess;
+import os,re,subprocess,json;
 
 # 无日志打印运行命令
 def runCmd(cmd, cwd=os.getcwd(), funcName="call"):
@@ -14,16 +14,41 @@ def runCmd(cmd, cwd=os.getcwd(), funcName="call"):
     return getattr(subprocess, funcName)(cmd, cwd = cwd, startupinfo = startupinfo);
 
 # 获取依赖模块表
-def getDependMods():
-    modList = [];
-    if not os.path.exists("depends.mod"):
+def getDependMods(assetsPath):
+    modList, modFile = [], os.path.join(assetsPath, "depends.mod");
+    if not os.path.exists(modFile):
         return modList;
-    with open("depends.mod", "r") as f:
+    with open(modFile, "r") as f:
         for line in f.readlines():
             mod = line.strip();
             if mod not in modList:
                 modList.append(mod);
     return modList;
+
+# 获取依赖模块数据
+def getDependMapJson():
+    dependJsonPath = os.path.join("data", "depend_map.json");
+    if os.path.exists(dependJsonPath):
+        with open(dependJsonPath, "r") as f:
+            return json.loads(f.read());
+    return {};
+
+# 设置依赖模块数据
+def setDependMapJson(dependMap):
+    dependJsonPath = os.path.join("data", "depend_map.json");
+    with open(dependJsonPath, "w") as f:
+        f.write(json.dumps(dependMap));
+
+# 获取依赖模块列表
+def checkDependMapJson(assetsPath):
+    isChange, dependMap = False, getDependMapJson();
+    for mod in getDependMods(assetsPath):
+        if mod not in dependMap:
+            dependMap[mod] = 1;
+            isChange = True;
+    if isChange:
+        setDependMapJson(dependMap);
+    return dependMap;
 
 # 获取已安装模块
 def getInstalledMods(pyExe):
@@ -36,10 +61,10 @@ def getInstalledMods(pyExe):
     return modList;
 
 # 获取未安装模块
-def getUninstalledMods(pyExe):
+def getUninstalledMods(pyExe, assetsPath):
     modList = getInstalledMods(pyExe); # 获取已安装模块
     unInstallMods = [];
-    for mod in getDependMods():
+    for mod in checkDependMapJson(assetsPath):
         if mod not in modList:
             unInstallMods.append(mod);
     return unInstallMods;
@@ -53,9 +78,9 @@ def installMods(pyExe, mods):
     return failedMods;
 
 # 安装依赖模块
-def installDepends(pyExe):
+def installDepends(pyExe, assetsPath):
     # 获取未安装模块
-    unInstallMods = getUninstalledMods(pyExe);
+    unInstallMods = getUninstalledMods(pyExe, assetsPath);
     # 安装未安装模块
     failedMods = installMods(pyExe, unInstallMods);
     if len(failedMods) > 0:
@@ -79,9 +104,10 @@ def getAssetsPath():
 if __name__ == '__main__':
     # 获取python依赖路径
     pyExe = os.path.abspath(os.path.join(os.getcwd(), "include", "python", "python.exe"));
-    # 安装依赖模块
-    installDepends(pyExe);
-    # 运行main文件
+    # 获取资源路径
     assetsPath = getAssetsPath();
+    # 安装依赖模块
+    installDepends(pyExe, assetsPath);
+    # 运行main文件
     runPath = os.path.abspath(os.path.join(os.getcwd(), "run"));
     runCmd(" ".join([os.path.join(runPath, "run.bat"), pyExe, assetsPath, getMainFile(assetsPath), os.getcwd()]), os.getcwd());
