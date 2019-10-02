487bcc2d2faa32420f77d48044fbdec55d3f1d82
JimDreamHeart <15602291936@163.com>
2019-10-02 11:59:01 +0800

    refactor:更新工具模板的工程配置

--- assets/template/assets/ProjectConfig.py
+++ assets/template/assets/ProjectConfig.py
@@ -8,5 +8,5 @@ ProjectConfig = {
 	"name" : "工具开发模板", # 工具名称
 	"version" : "1.0.0", # 工具版本
 	"winSize" : (960,640), # 模板窗口默认大小
-	"isOpenLogWin" : True, # 是否打开日志窗口
+	"isOpenLogWin" : False, # 是否打开日志窗口
 }
