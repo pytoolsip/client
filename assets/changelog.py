e8c749b9f641726481a37cf81281625ee2798632
JimDreamHeart <15602291936@163.com>
2020-02-05 21:22:57 +0800

    refactor:更新版本日志

--- assets/changelog.py
+++ assets/changelog.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 # @Author: JimZhang
 # @Date:   2020-02-05 09:58:17
-# @Last Modified by:   JimZhang
-# @Last Modified time: 2020-02-05 10:20:57
+# @Last Modified by:   JimDreamHeart
+# @Last Modified time: 2020-02-05 21:22:23
 
 ChangeLog = {
+	"1.0.18" : {
+		"time" : "2020-02-05 21:20:00",
+		"log" : """
+			refactor:
+				更新登录弹窗逻辑及界面；
+				启用退出平台菜单项。
+		""",
+	},
+	"1.0.17" : {
+		"time" : "2020-02-05 20:09:49",
+		"log" : """
+			fix: 修复登录平台报错问题。
+		""",
+	},
 	"1.0.16" : {
 		"time" : "2020-02-04 23:43:40",
 		"log" : """
